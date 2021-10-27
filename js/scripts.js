1fed51e0a848758d769b5a6acdcf00b35847bea7
Pablo Groba <pgroba@gmail.com>
2021-10-27 11:53:46 -0300

    Primer desafio

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//console.log('hola');
+const nombre = prompt('Ingrese su nombre');
+const edad = parseInt(prompt('Ingrese su edad'));
+
+console.log('Hola ' + nombre + '!. Nos alegra verte de nuevo!');
+console.log(
+  'Gracias a nuestra tecnologia de avanzada podremos saber cuantos años vas a tener dentro de un lustro!'
+);
+console.log('Tu edad sera: ' + (edad + 5) + ' años!!!');
+console.log('Igualmente que no decaiga!! Nosotros te queremos igual!');
