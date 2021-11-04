2d57ebb924a3e067adb6230e18a4e0294f6a8409
Pablo Groba <pgroba@gmail.com>
2021-11-04 20:02:24 -0300

    Tercer desafio

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,35 @@
 //console.log('hola');
-const nombre = prompt('Ingrese su nombre');
-const edad = parseInt(prompt('Ingrese su edad'));
-const edad_lustro = edad + 5;
+//const nombre = prompt('Ingrese su nombre');
+// const edad = parseInt(prompt('Ingrese su edad'));
+// const edad_lustro = edad + 5;
 
-console.log('Hola ' + nombre + '!. Nos alegra verte de nuevo!');
-console.log(
-  'Gracias a nuestra tecnologia de avanzada podremos saber cuantos años vas a tener dentro de un lustro!'
-);
-console.log('Tu edad sera: ' + edad_lustro + ' años!!!');
-if (edad_lustro <= 30) {
-  console.log('Vos todavia no te preocupas por nada, viva la vida loca!!');
+// console.log('Hola ' + nombre + '!. Nos alegra verte de nuevo!');
+// console.log(
+//   'Gracias a nuestra tecnologia de avanzada podremos saber cuantos años vas a tener dentro de un lustro!'
+// );
+// console.log('Tu edad sera: ' + edad_lustro + ' años!!!');
+// if (edad_lustro <= 30) {
+//   console.log('Vos todavia no te preocupas por nada, viva la vida loca!!');
+// } else {
+//   console.log(
+//     'Uhhh eso dolio, pero bueno, lo importante es que estamos vivos no? =)'
+//   );
+// }
+// if (nombre.toUpperCase() === 'ROBERTO') {
+//   console.log(
+//     'No importa lo que diga arriba, ya con este nombre, eres un viejo igual! JAJA con amor obvio'
+//   );
+// }
+
+let opcion = prompt('Desea saber cuantas letras tiene su nombre? Y/N');
+let nombre;
+if (opcion.toUpperCase() === 'Y' || opcion.toUpperCase() === 'N') {
+  while (opcion.toUpperCase() === 'Y') {
+    nombre = prompt('Ingrese su nombre');
+    alert(`Tu nombre tiene ${nombre.length} letra\s`);
+    opcion = prompt('Desea saber cuantas letras tiene otro nombre? Y/N');
+  }
+  alert('Lesto, terminamo aca!');
 } else {
-  console.log(
-    'Uhhh eso dolio, pero bueno, lo importante es que estamos vivos no? =)'
-  );
-}
-if (nombre.toUpperCase() === 'ROBERTO') {
-  console.log(
-    'No importa lo que diga arriba, ya con este nombre, eres un viejo igual! JAJA con amor obvio'
-  );
+  alert('Era simple loco, Y o N, te hiciste el piola, ahora se termino!');
 }
